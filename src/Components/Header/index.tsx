a220a44dec4096dc219461a82fe784280d7d99c4
L0ufelps <70579234+L0uFelps@users.noreply.github.com>
2023-05-17 16:05:22 -0300

    criaçao de botao de logout

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,6 +1,12 @@
 import './style.sass'
 import  logo  from '../../assets/img/logo.png'
+import  exit  from '../../assets/img/exitIcon.png'
+import { useNavigate } from 'react-router-dom';
 export default function Header(props: any) {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        navigate(-1);
+    }
 
     const navBar = () => {
         if (props.loged) {
@@ -26,6 +32,11 @@ export default function Header(props: any) {
         <header>
             <div className='header-logo'>
                 <img src={logo} alt="Logo"></img>
+            </div>
+            <div className="header-exit" onClick={handleLogout}>
+            <button>
+              <img src={exit} alt="Exit"></img>
+            </button>
             </div>
             <nav>
                 {navBar()}
